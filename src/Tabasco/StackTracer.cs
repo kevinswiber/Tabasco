8fc62e4bc5edf93566b588b77b5e6becbe3d3aca
Kevin Swiber <kswiber@gmail.com>
2011-02-07 17:47:59 -0500

    Adding Razor integration and updated sample.

---
+++ src/Tabasco/StackTracer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Tabasco
+{
+    public class StackTracer
+    {
+        public static string GetPreviousMethodName(MethodBase currentMethod)
+        {
+            var methodName = string.Empty;
+            try
+            {
+                var sTrace = new StackTrace(true);
+
+                //loop through all the stack frames
+                for (var frameCount = 0; frameCount < sTrace.FrameCount; frameCount++)
+                {
+                    var sFrame = sTrace.GetFrame(frameCount);
+                    var thisMethod = sFrame.GetMethod();
+
+                    //If the Type in the frame is the type that is being searched
+                    if (thisMethod != currentMethod)
+                    {
+                        continue;
+                    }
+
+                    if (frameCount + 1 <= sTrace.FrameCount)
+                    {
+                        var count = 1;
+                        do
+                        {
+                            var prevFrame = sTrace.GetFrame(frameCount + count);
+                            var prevMethod = prevFrame.GetMethod();
+                            methodName = prevMethod.ReflectedType + "." + prevMethod.Name;
+                            count++;
+                            //get the method and its parameter info
+                            //then exit out of the for loop
+                        } while (methodName == ".CallSite.Target" ||
+                                 methodName == "System.Dynamic.UpdateDelegates.UpdateAndExecute2");
+                    }
+                    break;
+                }
+            }
+            catch (Exception)
+            {
+                //swallow all exceptions this may encounter...this is informational and mroe for convenience anyways
+                return string.Empty;
+            }
+            return methodName;
+        }
+    }
+}
---
+++ src/Tabasco/ViewEngines/Razor/RazorView.cs
@@ -0,0 +1,11 @@
+using RazorEngine.Compilation;
+using RazorEngine.Templating;
+
+namespace Tabasco.ViewEngines.Razor
+{
+    [HasDynamicModel]
+    public class RazorView : TemplateBase<dynamic>
+    {
+
+    }
+}
